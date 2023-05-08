9930c6ab9c606eaf38eacf9ce96e0b6601126944
Isaac  Tingo <newtone480@gmail.com>
2023-05-08 13:53:02 +0300

    Setting up wefitter

---
+++ android/app/src/main/java/com/wefitter_test_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wefitter_test_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wefitter_test_app";
+  }
+}
